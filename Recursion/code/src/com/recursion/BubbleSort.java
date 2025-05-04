26fbed4c2e97be8d9fbb57c25ce7c4320455341c
Ajmal Ansari Ahamed Mohammed <sardarahmed110@gmail.com>
2025-05-04 16:27:46 +0530

    bubble sort --recursion

---
+++ Recursion/code/src/com/recursion/BubbleSort.java
@@ -0,0 +1,29 @@
+package com.recursion;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int r,int c,int[] arr){
+        if(r==0){
+            return;
+        }
+        if(c==r){
+            bubbleSort(r-1,0,arr);
+        }
+        else{
+            if(arr[c]>arr[c+1]){
+                int temp=arr[c];
+                arr[c]=arr[c+1];
+                arr[c+1]=temp;
+            }
+            bubbleSort(r,c+1,arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={5,4,3,2,1};
+        bubbleSort(arr.length-1,0,arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
